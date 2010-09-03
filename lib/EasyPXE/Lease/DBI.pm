0ecc96af133319b084665324002545da73812297
jeremy.cole <jeremy.cole@13193b08-1671-7b11-d27c-fc54a293f159>
2010-09-03 04:27:16 +0000

    Harmonize copyright lines.

--- lib/EasyPXE/Lease/DBI.pm
+++ lib/EasyPXE/Lease/DBI.pm
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 
-# Copyright (C) 2010 Jeremy Cole
+# Copyright (c) 2010 Jeremy Cole <jeremy@jcole.us>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
