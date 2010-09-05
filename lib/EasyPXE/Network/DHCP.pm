7bda368588fd62943d2a23dd3514329b47a656dd
jeremy.cole <jeremy.cole@13193b08-1671-7b11-d27c-fc54a293f159>
2010-09-05 00:22:39 +0000

    Rename DHCP files to DHCP.pm.

--- lib/EasyPXE/Network/DHCP.pm
+++ lib/EasyPXE/Network/DHCP.pm
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-package EasyPXE::Network::UDP;
+package EasyPXE::Network::DHCP;
 
 use strict;
 use warnings;
--- lib/EasyPXE/Protocol/DHCP.pm
+++ lib/EasyPXE/Protocol/DHCP.pm
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-package EasyPXE::Protocol::Standard;
+package EasyPXE::Protocol::DHCP;
 
 use strict;
 use warnings;
